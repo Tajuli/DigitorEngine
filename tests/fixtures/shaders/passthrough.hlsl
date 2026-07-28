114969cada5b951735c8abb0ee6ba74e07211041
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-28 11:57:21 +0600

    Delete tests directory

--- tests/fixtures/shaders/passthrough.hlsl
+++
@@ -1,2 +0,0 @@
-Texture2D<float4> source_texture : register(t0); SamplerState source_sampler : register(s1);
-float4 main(float2 uv:TEXCOORD0):SV_Target { return source_texture.Sample(source_sampler,uv); }
