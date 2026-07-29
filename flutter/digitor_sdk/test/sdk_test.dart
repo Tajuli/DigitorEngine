9b264c61ea85af0db60da7bb565dc0ee91fb5f67
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-29 21:46:53 +0600

    Delete flutter/digitor_sdk directory

--- flutter/digitor_sdk/test/sdk_test.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:digitor_sdk/digitor_sdk.dart';
-void main(){test('validates async calls',()async{final sdk=DigitorSdk('missing');await sdk.seek(10);await expectLater(sdk.seek(-1),throwsArgumentError);});}
