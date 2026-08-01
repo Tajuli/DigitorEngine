71be993f28714d7744ae8863391def757243f764
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-01 15:05:14 +0600

    Format Dart FFI bindings

--- dart/digitor_engine_ffi/lib/src/bindings.dart
+++ dart/digitor_engine_ffi/lib/src/bindings.dart
@@ -83,33 +83,89 @@ typedef _CreateDart = int Function(
   Pointer<DigitorTimelineSessionConfigNative>,
   Pointer<Pointer<DigitorTimelineAudioSession>>,
 );
-typedef _DestroyNative = Int32 Function(Pointer<DigitorTimelineAudioSession>);
+typedef _DestroyNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+);
 typedef _DestroyDart = int Function(Pointer<DigitorTimelineAudioSession>);
-typedef _PublishNative = Int32 Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelinePublicationNative>);
-typedef _PublishDart = int Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelinePublicationNative>);
-typedef _CommandNative = Int32 Function(Pointer<DigitorTimelineAudioSession>);
+typedef _PublishNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelinePublicationNative>,
+);
+typedef _PublishDart = int Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelinePublicationNative>,
+);
+typedef _CommandNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+);
 typedef _CommandDart = int Function(Pointer<DigitorTimelineAudioSession>);
-typedef _SeekNative = Int32 Function(Pointer<DigitorTimelineAudioSession>, Int64);
-typedef _SeekDart = int Function(Pointer<DigitorTimelineAudioSession>, int);
-typedef _ControlsNative = Int32 Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorAudioSessionControlsNative>);
-typedef _ControlsDart = int Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorAudioSessionControlsNative>);
-typedef _StatusNative = Int32 Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelineSessionStatusNative>);
-typedef _StatusDart = int Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelineSessionStatusNative>);
-typedef _TelemetryNative = Int32 Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelineSessionTelemetryNative>);
-typedef _TelemetryDart = int Function(Pointer<DigitorTimelineAudioSession>, Pointer<DigitorTimelineSessionTelemetryNative>);
+typedef _SeekNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Int64,
+);
+typedef _SeekDart = int Function(
+  Pointer<DigitorTimelineAudioSession>,
+  int,
+);
+typedef _ControlsNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorAudioSessionControlsNative>,
+);
+typedef _ControlsDart = int Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorAudioSessionControlsNative>,
+);
+typedef _StatusNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelineSessionStatusNative>,
+);
+typedef _StatusDart = int Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelineSessionStatusNative>,
+);
+typedef _TelemetryNative = Int32 Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelineSessionTelemetryNative>,
+);
+typedef _TelemetryDart = int Function(
+  Pointer<DigitorTimelineAudioSession>,
+  Pointer<DigitorTimelineSessionTelemetryNative>,
+);
 
 final class DigitorTimelineBindings {
   DigitorTimelineBindings(DynamicLibrary library)
-      : create = library.lookupFunction<_CreateNative, _CreateDart>('digitor_timeline_session_create'),
-        destroy = library.lookupFunction<_DestroyNative, _DestroyDart>('digitor_timeline_session_destroy'),
-        publish = library.lookupFunction<_PublishNative, _PublishDart>('digitor_timeline_session_publish'),
-        play = library.lookupFunction<_CommandNative, _CommandDart>('digitor_timeline_session_play'),
-        pause = library.lookupFunction<_CommandNative, _CommandDart>('digitor_timeline_session_pause'),
-        stop = library.lookupFunction<_CommandNative, _CommandDart>('digitor_timeline_session_stop'),
-        seek = library.lookupFunction<_SeekNative, _SeekDart>('digitor_timeline_session_seek'),
-        setAudioControls = library.lookupFunction<_ControlsNative, _ControlsDart>('digitor_timeline_session_set_audio_controls'),
-        getStatus = library.lookupFunction<_StatusNative, _StatusDart>('digitor_timeline_session_get_status'),
-        getTelemetry = library.lookupFunction<_TelemetryNative, _TelemetryDart>('digitor_timeline_session_get_telemetry');
+      : create = library.lookupFunction<_CreateNative, _CreateDart>(
+          'digitor_timeline_session_create',
+        ),
+        destroy = library.lookupFunction<_DestroyNative, _DestroyDart>(
+          'digitor_timeline_session_destroy',
+        ),
+        publish = library.lookupFunction<_PublishNative, _PublishDart>(
+          'digitor_timeline_session_publish',
+        ),
+        play = library.lookupFunction<_CommandNative, _CommandDart>(
+          'digitor_timeline_session_play',
+        ),
+        pause = library.lookupFunction<_CommandNative, _CommandDart>(
+          'digitor_timeline_session_pause',
+        ),
+        stop = library.lookupFunction<_CommandNative, _CommandDart>(
+          'digitor_timeline_session_stop',
+        ),
+        seek = library.lookupFunction<_SeekNative, _SeekDart>(
+          'digitor_timeline_session_seek',
+        ),
+        setAudioControls = library.lookupFunction<
+          _ControlsNative,
+          _ControlsDart
+        >('digitor_timeline_session_set_audio_controls'),
+        getStatus = library.lookupFunction<_StatusNative, _StatusDart>(
+          'digitor_timeline_session_get_status',
+        ),
+        getTelemetry = library.lookupFunction<
+          _TelemetryNative,
+          _TelemetryDart
+        >('digitor_timeline_session_get_telemetry');
 
   final _CreateDart create;
   final _DestroyDart destroy;
