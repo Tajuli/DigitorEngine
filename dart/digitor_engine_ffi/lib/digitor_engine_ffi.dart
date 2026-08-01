e03ccfb6f2a8c5d1dbc17a8e28acd021e08fe77d
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-01 22:23:44 +0600

    Export Dart audio sync API

--- dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
+++ dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
@@ -1,3 +1,4 @@
+export 'src/audio_sync.dart';
 export 'src/bindings.dart' show DigitorTimelineBindings;
 export 'src/library_loader.dart' show DigitorLibraryLoader;
 export 'src/sdk_callbacks.dart';
