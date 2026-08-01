c2d245bbb1e3fdd1de6f43c2aab06cbff9d16979
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-01 14:54:16 +0600

    Add Dart ABI binding tests

---
+++ dart/digitor_engine_ffi/test/bindings_test.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+import 'package:digitor_engine_ffi/src/bindings.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('native structs have stable non-zero layouts', () {
+    expect(sizeOf<DigitorTimelineSessionConfigNative>(), greaterThanOrEqualTo(16));
+    expect(sizeOf<DigitorTimelinePublicationNative>(), greaterThanOrEqualTo(24));
+    expect(sizeOf<DigitorAudioSessionControlsNative>(), greaterThanOrEqualTo(18));
+    expect(sizeOf<DigitorTimelineSessionStatusNative>(), greaterThanOrEqualTo(64));
+    expect(sizeOf<DigitorTimelineSessionTelemetryNative>(), equals(56));
+  });
+
+  test('opaque timeline session pointer can be represented', () {
+    final pointer = Pointer<DigitorTimelineAudioSession>.fromAddress(1);
+    expect(pointer.address, 1);
+  });
+}
