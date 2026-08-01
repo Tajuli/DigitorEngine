fc6dc93e20079457f3ae1c94fbe8d282e8cae2c3
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-01 14:53:16 +0600

    Export Dart session API

---
+++ bindings/dart/digitor_engine/lib/digitor_engine.dart
@@ -0,0 +1,13 @@
+library digitor_engine;
+
+export 'src/native_api.dart'
+    show
+        DigitorAudioControls,
+        DigitorNativeApi,
+        DigitorPlaybackState,
+        DigitorSessionConfig,
+        DigitorSessionStatus,
+        DigitorSessionTelemetry,
+        FfiDigitorNativeApi;
+export 'src/timeline_audio_session.dart'
+    show DigitorEngineException, DigitorTimelineAudioSession;
