afc3c4037475285abfda2b2b6194e8938dadeffb
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-02 19:31:53 +0600

    Enforce zero-copy timeline adapter boundaries

--- include/digitor/timeline_media_adapter.hpp
+++ include/digitor/timeline_media_adapter.hpp
@@ -27,6 +27,7 @@ struct MediaDecodeRequest {
   std::uint32_t height{};
   bool proxy{};
   TimelineExecutionMode mode{TimelineExecutionMode::preview};
+  bool require_zero_copy{};
 };
 
 struct MediaAdapterCallbacks {
@@ -34,6 +35,9 @@ struct MediaAdapterCallbacks {
   std::function<std::optional<RenderAudioBlock>(const MediaDecodeRequest&, std::size_t)> decode_audio;
   std::function<bool(const VideoExecutionLayer&, RenderVideoFrame&)> apply_effects;
   std::function<bool(const VideoExecutionLayer&, const RenderVideoFrame&, RenderVideoFrame&)> composite;
+  std::function<std::optional<RenderVideoFrame>(std::uint32_t width,
+                                                std::uint32_t height,
+                                                std::int64_t timestamp_us)> create_gpu_target;
   std::function<bool(const RenderVideoFrame&, const TimelineExecutionPlan&)> deliver_preview;
   std::function<bool(const RenderVideoFrame&, const TimelineExecutionPlan&)> deliver_export;
   std::function<bool()> cancelled;
@@ -41,7 +45,9 @@ struct MediaAdapterCallbacks {
 
 class TimelineMediaAdapter {
  public:
-  TimelineMediaAdapter(std::vector<TimelineMediaSource> sources, MediaAdapterCallbacks callbacks);
+  TimelineMediaAdapter(std::vector<TimelineMediaSource> sources,
+                       MediaAdapterCallbacks callbacks,
+                       bool require_zero_copy = false);
 
   [[nodiscard]] TimelineRenderCallbacks make_render_callbacks(TimelineExecutionMode mode,
                                                                std::uint32_t width,
@@ -53,10 +59,12 @@ class TimelineMediaAdapter {
   [[nodiscard]] std::optional<std::string> selected_path(const std::string& clip_id,
                                                          TimelineExecutionMode mode,
                                                          bool allow_proxy = true) const;
+  [[nodiscard]] bool requires_zero_copy() const noexcept { return require_zero_copy_; }
 
  private:
   std::unordered_map<std::string, TimelineMediaSource> sources_;
   MediaAdapterCallbacks callbacks_;
+  bool require_zero_copy_{};
 };
 
 }  // namespace digitor
