43bb690251b3d4d2df57cd17819cb5277c44f3f9
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-03 13:15:40 +0600

    Add Apple native provider contract

---
+++ include/digitor/apple_native_provider.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "digitor/native_platform_provider_fixed.hpp"
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+namespace digitor {
+
+struct AppleFlutterTextureBridge final {
+  const void* flutter_texture_registrar{};
+  std::string implementation_identity;
+  std::function<bool()> attached;
+  std::function<DigitorResult(const ProcessedGpuFramePtr&, std::uint64_t)> present;
+};
+
+struct AppleNativeProviderCapabilities final {
+  bool metal{};
+  bool video_toolbox{};
+  bool core_video{};
+  bool core_media{};
+  bool iosurface{};
+  bool cvmetal_texture_cache{};
+  bool hardware_encoder{};
+  bool iosurface_pixel_buffer_pool{};
+  bool flutter_texture_bridge{};
+  bool metal_completion_sync{};
+  bool lifecycle_safe{};
+  bool hdr_attachments{};
+  bool zero_cpu_readback{};
+  bool zero_cpu_staging{};
+  std::string device_identity;
+  std::string encoder_identity;
+};
+
+struct AppleNativeProviderBindings final {
+  ProductionPlatform platform{ProductionPlatform::macos};
+  ProductionTimelineGpuHost timeline;
+  AppleFlutterTextureBridge flutter;
+  AppleHardwareEncoderHost encoder;
+  AppleNativeProviderCapabilities capabilities;
+  const void* device_identity{};
+  std::string package_identity;
+  std::string build_identity;
+};
+
+struct AppleNativeProviderResult final {
+  NativePlatformProvider provider;
+  DigitorResult result{DIGITOR_RESULT_INVALID_ARGUMENT};
+  std::string diagnostic;
+  explicit operator bool() const noexcept { return result == DIGITOR_RESULT_OK; }
+};
+
+[[nodiscard]] AppleNativeProviderResult create_apple_native_provider(
+    AppleNativeProviderBindings bindings) noexcept;
+
+}  // namespace digitor
