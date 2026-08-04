b9b7c92fa1563ad6e7e1c8edddac9fdf74910d91
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-05 02:36:19 +0600

    Harden native text pipeline ownership

--- include/digitor/native_text_pipeline.hpp
+++ include/digitor/native_text_pipeline.hpp
@@ -4,7 +4,6 @@
 #include <functional>
 #include <optional>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 namespace digitor {
@@ -12,87 +11,25 @@ namespace digitor {
 enum class TextDirection : std::uint32_t { auto_detect, left_to_right, right_to_left };
 enum class TextScript : std::uint32_t { auto_detect, latin, bengali, arabic, devanagari, common };
 
-struct FontDescriptor {
-  std::string family;
-  std::string path;
-  std::uint32_t face_index{};
-  std::uint32_t weight{400};
-  bool italic{};
-};
-
-struct ShapingRequest {
-  std::string utf8;
-  FontDescriptor primary_font;
-  std::vector<FontDescriptor> fallback_fonts;
-  TextDirection direction{TextDirection::auto_detect};
-  TextScript script{TextScript::auto_detect};
-  std::string language;
-  double font_size{48.0};
-  double letter_spacing{};
-};
-
-struct ShapedGlyph {
-  std::uint32_t glyph_id{};
-  std::uint32_t cluster{};
-  std::uint32_t font_slot{};
-  double x_advance{};
-  double y_advance{};
-  double x_offset{};
-  double y_offset{};
-};
-
-struct ShapedRun {
-  std::vector<ShapedGlyph> glyphs;
-  TextDirection direction{TextDirection::left_to_right};
-  TextScript script{TextScript::common};
-  double advance_x{};
-  double advance_y{};
-  bool used_complex_shaping{};
-  bool used_fallback{};
-};
-
-struct GlyphBitmap {
-  std::uint32_t width{};
-  std::uint32_t height{};
-  std::int32_t bearing_x{};
-  std::int32_t bearing_y{};
-  std::int32_t advance_x{};
-  std::vector<std::uint8_t> coverage;
-};
-
-struct AtlasEntry {
-  std::uint32_t page{};
-  std::uint32_t x{};
-  std::uint32_t y{};
-  std::uint32_t width{};
-  std::uint32_t height{};
-  float u0{};
-  float v0{};
-  float u1{};
-  float v1{};
-};
-
-struct TextVertex {
-  float x{};
-  float y{};
-  float u{};
-  float v{};
-  std::uint32_t rgba{0xffffffffu};
-  std::uint32_t page{};
-};
-
-struct TextDrawPacket {
-  std::vector<TextVertex> vertices;
-  std::vector<std::uint32_t> indices;
-  std::uint32_t atlas_generation{};
-  bool gpu_ready{};
-};
+struct FontDescriptor { std::string family; std::string path; std::uint32_t face_index{}; std::uint32_t weight{400}; bool italic{}; };
+struct ShapingRequest { std::string utf8; FontDescriptor primary_font; std::vector<FontDescriptor> fallback_fonts; TextDirection direction{TextDirection::auto_detect}; TextScript script{TextScript::auto_detect}; std::string language; double font_size{48.0}; double letter_spacing{}; };
+struct ShapedGlyph { std::uint32_t glyph_id{}; std::uint32_t cluster{}; std::uint32_t font_slot{}; double x_advance{}; double y_advance{}; double x_offset{}; double y_offset{}; };
+struct ShapedRun { std::vector<ShapedGlyph> glyphs; TextDirection direction{TextDirection::left_to_right}; TextScript script{TextScript::common}; double advance_x{}; double advance_y{}; bool used_complex_shaping{}; bool used_fallback{}; };
+struct GlyphBitmap { std::uint32_t width{}; std::uint32_t height{}; std::int32_t bearing_x{}; std::int32_t bearing_y{}; std::int32_t advance_x{}; std::vector<std::uint8_t> coverage; };
+struct AtlasEntry { std::uint32_t page{}; std::uint32_t x{}; std::uint32_t y{}; std::uint32_t width{}; std::uint32_t height{}; float u0{}; float v0{}; float u1{}; float v1{}; };
+struct TextVertex { float x{}; float y{}; float u{}; float v{}; std::uint32_t rgba{0xffffffffu}; std::uint32_t page{}; };
+struct TextDrawPacket { std::vector<TextVertex> vertices; std::vector<std::uint32_t> indices; std::uint32_t atlas_generation{}; bool gpu_ready{}; };
 
 using GlyphRasterizer = std::function<std::optional<GlyphBitmap>(const FontDescriptor&, std::uint32_t, double)>;
 
 class NativeTextPipeline {
  public:
   explicit NativeTextPipeline(std::uint32_t atlas_width = 1024, std::uint32_t atlas_height = 1024);
+  ~NativeTextPipeline();
+  NativeTextPipeline(const NativeTextPipeline&) = delete;
+  NativeTextPipeline& operator=(const NativeTextPipeline&) = delete;
+  NativeTextPipeline(NativeTextPipeline&&) = delete;
+  NativeTextPipeline& operator=(NativeTextPipeline&&) = delete;
   ShapedRun shape(const ShapingRequest&) const;
   TextDrawPacket prepare(const ShapingRequest&, const GlyphRasterizer&, std::uint32_t rgba = 0xffffffffu);
   std::uint32_t atlas_generation() const noexcept;
