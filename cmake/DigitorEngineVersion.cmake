2c3ba8ebef9ff660252819d15c3446c21060bb87
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-06 23:55:29 +0600

    Load native image runtime production wiring

--- cmake/DigitorEngineVersion.cmake
+++ cmake/DigitorEngineVersion.cmake
@@ -8,3 +8,8 @@ list(GET _digitor_version_parts 0 DIGITOR_ENGINE_VERSION_MAJOR)
 list(GET _digitor_version_parts 1 DIGITOR_ENGINE_VERSION_MINOR)
 list(GET _digitor_version_parts 2 DIGITOR_ENGINE_VERSION_PATCH)
 unset(_digitor_version_parts)
+
+# CMakeLists creates and extends digitor_engine after this canonical version file
+# is included. Defer platform image source/framework wiring until directory end.
+cmake_language(DEFER CALL include
+    "${CMAKE_CURRENT_LIST_DIR}/DigitorNativeImageRuntime.cmake")
