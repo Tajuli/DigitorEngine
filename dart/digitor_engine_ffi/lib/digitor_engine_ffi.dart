a7033e079af4fd3a241e1446992aea1c37b3d9b3
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-08 00:29:48 +0600

    Export internal engine and node graph adapters

--- dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
+++ dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
@@ -3,6 +3,8 @@ export 'src/live_playback_sync.dart';
 export 'src/bindings.dart' show DigitorTimelineBindings;
 export 'src/export_job.dart';
 export 'src/library_loader.dart' show DigitorLibraryLoader;
+export 'src/native_engine.dart';
+export 'src/node_graph.dart';
 export 'src/sdk_callbacks.dart';
 export 'src/sdk_worker.dart';
 export 'src/session.dart';
