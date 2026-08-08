78eb910bd86c475dfa5c4b8aabe374fbbfe85089
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-09 04:40:03 +0600

    Export engine-owned editor workspace

--- dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
+++ dart/digitor_engine_ffi/lib/digitor_engine_ffi.dart
@@ -1,6 +1,7 @@
 export 'src/audio_sync.dart';
 export 'src/live_playback_sync.dart';
 export 'src/bindings.dart' show DigitorTimelineBindings;
+export 'src/editor_workspace.dart';
 export 'src/engine.dart';
 export 'src/export_job.dart';
 export 'src/library_loader.dart' show DigitorLibraryLoader;
