4ac19fa3368f24e013b2afa6a257ab15b1dd75fa
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-10 00:34:05 +0600

    Test timeline ABI through packaged native asset

--- dart/digitor_engine_ffi/test/bindings_test.dart
+++ dart/digitor_engine_ffi/test/bindings_test.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:digitor_engine_ffi/src/bindings.dart';
+import 'package:digitor_engine_ffi/src/session.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -27,5 +28,14 @@ void main() {
   test('opaque timeline session pointer can be represented', () {
     final pointer = Pointer<DigitorTimelineAudioSession>.fromAddress(1);
     expect(pointer.address, 1);
+  });
+
+  test('packaged native asset resolves timeline session ABI', () {
+    final session = DigitorTimelineSession.create(
+      sampleRate: 48000,
+      channels: 2,
+      durationUs: 1000000,
+    );
+    session.dispose();
   });
 }
